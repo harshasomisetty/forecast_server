e17a1f626fec22337b5577b6bdacb814a358fb7c
Harsha Somisetty <harshasomisetty7@gmail.com>
2022-01-27 21:49:14 -0500

    init server

---
+++ server.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+
+@routes.get('/')
+async def handle_main(request):
+    text = "Main"
+    return web.Response(text=text)
+
+@routes.get('/name/{name}')
+async def handle_get(request):
+    name = request.match_info.get('name', "Anonymous")
+    text = "Hello, " + name
+    return web.Response(text=text)
+
+@routes.post('/post')
+async def handle_post(request):
+    data = await request.post()
+    # print(data.values())
+    print(data.len())
+    return web.Response(text="hi")
+
+app = web.Application()
+app.add_routes(routes)
+
+
+
+if __name__ == '__main__':
+    web.run_app(app, port=8080)
