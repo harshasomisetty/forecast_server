ca347fac1214db5abf45f88de99df721365c8566
Harsha Somisetty <harshasomisetty7@gmail.com>
2022-01-28 13:14:11 -0500

    got interpolation with scikit working for all keys

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 import pandas as pd
+from pandas.tseries.offsets import *
+import numpy as np
 from aiohttp import web
+from sklearn.linear_model import LinearRegression
 
 routes = web.RouteTableDef()
 
@@ -15,11 +18,19 @@ async def handle_get(request):
     text = "Hello, " + name
     return web.Response(text=text)
 
-@routes.post('/post')
+@routes.post('/forecast')
 async def handle_post(request):
     data = await request.json()
-    print(data.keys())
-    # print(data.len())
+    keys = list(data.keys())
+    values = {}
+
+    for key in keys:
+        vals = np.array(list(data[key].values()))
+        vals = vals[vals != np.array(None)]
+        # print(vals)
+        model = LinearRegression().fit(np.arange(0, vals.size).reshape(-1, 1), vals)
+        values[key] = model.predict(np.array([vals.size]).reshape(-1, 1))[0]
+    print(values)
     return web.Response(text="hi")
 
 app = web.Application()
