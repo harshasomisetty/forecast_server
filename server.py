55b62a4e6c1f8c6c355031d028561926a64d7e08
Harsha Somisetty <harshasomisetty7@gmail.com>
2022-01-29 10:38:25 -0500

    commenting code

--- server.py
+++ server.py
@@ -12,38 +12,43 @@ logging.basicConfig(level=logging.DEBUG, filename='forecasts.log',
 logging.getLogger("aiohttp").setLevel(logging.WARNING)
 
 async def handle_forecast(request):
-    try:
+    try: #makes sure post request contains a payload
         data = await request.json()
     except:
         return web.Response(text="Error: No Data in Post")
     predictions = {}
 
-    for key in data.keys():
+    for key in data.keys(): #tries to iterate through each equity in payload
         try:
+            # ignore all null values
             prices = np.array([v for v in data[key].values() if v != None])
-        except AttributeError:
+        except AttributeError: # the payload json format is incorrect, so exit
             logging.warning("invalid data format: "+ str(data[key]))
             return web.Response(text="Error Invalid Data Format: "+key)
         
-        try:
+        try: 
             next_date = pd.to_datetime(pd.Series(data[key]).index[-1]) + BDay()
         except dateutil.parser._parser.ParserError:
             logging.warning("invalid string format "+ str(data[key]))
             return web.Response(text="Error Invalid String Format: "+key)
         
-        if prices.size <=1:
+        if prices.size <=1: # not enough data to make a prediction
             predictions[key] = {str(next_date) : None}
-        else:
+        else: # contruct lin reg model, make 1 step prediction the next result
+            
+            # evenly spacing all data points regardless of exact date to make prediction
             model = LinearRegression().fit(np.arange(0, prices.size).reshape(-1, 1), prices)
             predictions[key] = {str(next_date):
                                 model.predict(np.array([prices.size]).reshape(-1, 1))[0]}
 
         logging.info(predictions[key])
-            
-    return web.json_response(predictions)
+
+    return web.json_response(predictions) # final list of predictions for each equity
 
 
 app = web.Application()
+
+# all calls get routed to default endpoint function "handle forecast"
 app.add_routes([web.post('/{tail:.*}', handle_forecast)])
                 
 
